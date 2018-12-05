62c429ce0d1a9c0bc39ee26744028fe288dd0ba0
Bhatt1028 <43218176+Bhatt1028@users.noreply.github.com>
2018-12-05 14:36:29 -0600

    quiz is done
    awt

--- fun.js
+++ fun.js
@@ -6,26 +6,24 @@ function checkAnswers()
     var Q1Answer = document.quiz.a1.value.toLowerCase();
     if (Q1Answer === 'beedle')
     {
-        // increase score by one
-        score++; // same as score = score +1
-        //push some feedback
+        score++;
         feedback.push("Question 1 is correct!!");
-    }
-    else if (Q1Answer === 'beadle')
-    {
-        feedback.push("Almost, but that's just his favorite animal");
     }
     else if (Q1Answer === 'beatle')
     {
-        feedback.push("No, not that kind...");
+        feedback.push("Question 1 was incorrect!! No, not those beatles...");
+    }
+    else if (Q1Answer === 'beetle')
+    {
+        feedback.push("Question 1 was incorrect!! No, not those beatles...");
     }
     else if (Q1Answer === 'ganondorf')
     {
-        feedback.push("With a face of terror like that you might think...");
+        feedback.push("Question 1 was incorrect!! But with a face of terror like that, you might think...");
     }
     else
     {
-        feedback.push("Incorrect!! Question 1 was Beedle!");
+        feedback.push("Question 1 was incorrect!! Beedle was the answer!");
     }
 
     if (document.quiz.a2A.checked === false && document.quiz.a2B.checked === true && document.quiz.a2C.checked === true && document.quiz.a2D.checked === true && document.quiz.a2E.checked === false && document.quiz.a2F.checked === true)
@@ -35,7 +33,7 @@ function checkAnswers()
     }
     else
     {
-        feedback.push("Incorrect! It was = SS,ST,WW,BotW!")
+        feedback.push("Question 2 was incorrect!! It was = SS,ST,WW,BotW!")
     }
 
     if (document.quiz.a3.value === 'mer')
@@ -45,7 +43,7 @@ function checkAnswers()
     }
     else
     {
-        feedback.push("Incorrect!")
+        feedback.push("Question 3 was incorrect!! He's a merchant!")
     }
 
     if (document.quiz.a4.value === 'hc')
@@ -55,7 +53,7 @@ function checkAnswers()
     }
     else
     {
-        feedback.push("Incorrect!")
+        feedback.push("Question 4 was incorrect!! His favorite animal was the Horned Colossus Beetle!")
     }
 
     var Q5Answer = document.quiz.a5.value.toLowerCase();
@@ -66,21 +64,64 @@ function checkAnswers()
     }
     else
     {
-        feedback.push("Incorrect!! ");
+        feedback.push("Question 5 was incorrect!! It's LINK!");
     }
 
-    if (document.quiz.a6A.checked === true && document.quiz.a6B.checked === true && document.quiz.a6C.checked === true && document.quiz.a6D.checked === true && document.quiz.a6E.checked === true )
+    if (document.quiz.a6A.checked === true && document.quiz.a6B.checked === true && document.quiz.a6C.checked === false && document.quiz.a6D.checked === true && document.quiz.a6E.checked === false )
     {
         score++;
-        feedback.push("Question 3 is correct!!");
+        feedback.push("Question 6 is correct!!");
     }
     else
     {
-        feedback.push("Incorrect!")
+        feedback.push("Question 6 was incorrect!! You could obtain arrows, insects, and potions from him!")
     }
 
 
-    alert("Your score was... " + score + "!");
-    alert(feedback);
+    if (score <= 2)
+    {
+        alert("Your score was... " + score + ". Pathetic.");
+        alert(feedback[0]);
+        alert(feedback[1]);
+        alert(feedback[2]);
+        alert(feedback[3]);
+        alert(feedback[4]);
+        alert(feedback[5]);
+        alert("...is that even possible? Try harder!")
+    }
+    else if (score <= 4)
+    {
+        alert("Your score was... " + score + ".");
+        alert(feedback[0]);
+        alert(feedback[1]);
+        alert(feedback[2]);
+        alert(feedback[3]);
+        alert(feedback[4]);
+        alert(feedback[5]);
+    }
+    else if (score === 5)
+    {
+        alert("Your score was... " + score + "!");
+        alert(feedback[0]);
+        alert(feedback[1]);
+        alert(feedback[2]);
+        alert(feedback[3]);
+        alert(feedback[4]);
+        alert(feedback[5]);
+        alert("That's a pretty good score!")
+    }
+    else
+    {
+        alert("Your score was... " + score + "! Nice job!");
+        alert(feedback[0]);
+        alert(feedback[1]);
+        alert(feedback[2]);
+        alert(feedback[3]);
+        alert(feedback[4]);
+        alert(feedback[5]);
+        alert("You're a Beetle loving legend! (Or maybe you cheated by looking at the code, or took the quiz twice?.. Eh, Whatever.)")
+    }
+
+
 
 }
